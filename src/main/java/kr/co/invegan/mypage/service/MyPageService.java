0c0b8d3e341e563179c80cf9b80ba7d8fdd0e846
Yoonyoung Jang <kias3036@gmail.com>
2023-11-01 09:35:44 +0900

    mypage1

--- src/main/java/kr/co/invegan/mypage/service/MyPageService.java
+++ src/main/java/kr/co/invegan/mypage/service/MyPageService.java
@@ -1,8 +1,23 @@
 package kr.co.invegan.mypage.service;
 
+import java.util.ArrayList;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import kr.co.invegan.mypage.dao.MyPageDAO;
+import kr.co.invegan.mypage.dto.MyPageDTO;
 
 @Service
 public class MyPageService {
 
+	Logger logger = LoggerFactory.getLogger(getClass());
+	@Autowired MyPageDAO dao;
+	
+	public ArrayList<MyPageDTO> userInfo() {
+		
+		return dao.userInfo();
+	}
 }
