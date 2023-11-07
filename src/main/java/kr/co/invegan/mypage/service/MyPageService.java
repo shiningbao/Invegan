9112f3c4752eccf54837ba2e3e3ff5997479cfcc
Yoonyoung Jang <kias3036@gmail.com>
2023-11-08 00:34:19 +0900

    회원정보수정 사진등록과 수정, 캘린더 년도 select 수정 후 커밋

--- src/main/java/kr/co/invegan/mypage/service/MyPageService.java
+++ src/main/java/kr/co/invegan/mypage/service/MyPageService.java
@@ -1,5 +1,9 @@
 package kr.co.invegan.mypage.service;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -8,8 +12,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import kr.co.invegan.board.dto.PhotoDTO;
 import kr.co.invegan.mypage.dao.MyPageDAO;
 import kr.co.invegan.mypage.dto.MyPageDTO;
 
@@ -17,40 +21,39 @@ import kr.co.invegan.mypage.dto.MyPageDTO;
 public class MyPageService {
 
 	Logger logger = LoggerFactory.getLogger(getClass());
-	@Autowired MyPageDAO dao;
-	
-	PhotoDTO photodto = new PhotoDTO();
-	
+	@Autowired
+	MyPageDAO dao;
+
 	private String root = "C:/upload/";
-	
+
 	public MyPageDTO userInfo(int user_no) {
-		
+
 		return dao.userInfo(user_no);
 	}
 
 	public boolean overlay(String nickname) {
 		boolean use = false;
 		int cnt = dao.overlay(nickname);
-		if(cnt==0) {
+		if (cnt == 0) {
 			use = true;
 		}
 		return use;
 	}
 
 	public HashMap<String, Object> requestBoardList(int user_no, String page) {
-		
+
 		int p = Integer.parseInt(page);
-		int offset = (p-1)*5;
-		logger.info("p:"+p+"offset:"+offset);
-		ArrayList<MyPageDTO> pagingList = dao.requestBoardList(user_no,5,offset);
-		
+		int offset = (p - 1) * 5;
+		logger.info("p:" + p + "offset:" + offset);
+		ArrayList<MyPageDTO> pagingList = dao.requestBoardList(user_no, 5, offset);
+
 		HashMap<String, Object> map = new HashMap<String, Object>();
-		
+
 		int pages = dao.totalPage(5);
-		logger.info("만들 수 있는 총 페이지 갯수 : "+pages);
-		map.put("pages",pages); // 만들 페이지 수 
-		map.put("pagingList", pagingList);		
-		
+		logger.info("만들 수 있는 총 페이지 갯수 : " + pages);
+		map.put("pages", pages); // 만들 페이지 수
+		map.put("pagingList", pagingList);
+
 		return map;
 	}
 
@@ -58,7 +61,7 @@ public class MyPageService {
 	 * public HashMap<String, Object> recipeBoardList(int user_no, String page) {
 	 * return dao.recipeBoardList(user_no); }
 	 */
-	
+
 	/*
 	 * public MyPageDTO freeBoardList(int user_no) { return
 	 * dao.freeBoardList(user_no); }
@@ -95,12 +98,12 @@ public class MyPageService {
 	 * dao.restaurantFavorite(user_no); }
 	 */
 
-	public int updateNickname(String nickname, Integer user_no) {	
-		return dao.updateNickname(nickname,user_no);
+	public int updateNickname(String nickname, Integer user_no) {
+		return dao.updateNickname(nickname, user_no);
 	}
 
 	public String pwConfirm(String pw, Integer user_no) {
-		return dao.pwConfirm(pw,user_no);
+		return dao.pwConfirm(pw, user_no);
 	}
 
 	public int completePw(Map<String, Object> params) {
@@ -111,53 +114,82 @@ public class MyPageService {
 		return dao.save(params);
 	}
 
-	/*
-	 * public void imgUpload(MultipartFile photos, Integer user_no) throws
-	 * IOException {
-	 * 
-	 * MyPageDTO dto = new MyPageDTO(); dto.setUser_no(user_no);
-	 * dao.uploadProfile(dto);
-	 * 
-	 * int idx = dto.getUser_no();
-	 * 
-	 * 
-	 * if(idx>0) { saveFile(photos,idx,user_no); }
-	 * 
-	 * }
-	 * 
-	 * private void saveFile(MultipartFile photos, int idx, Integer user_no) throws
-	 * IOException { String oriFileName = photos.getOriginalFilename();
-	 * logger.info(oriFileName);
-	 * 
-	 * if(!oriFileName.equals("")) { String ext =
-	 * oriFileName.substring(oriFileName.lastIndexOf(".")); String server_file_name
-	 * = System.currentTimeMillis()+ext;
-	 * 
-	 * // 파일 저장 byte[] arr = photos.getBytes(); Path path =
-	 * Paths.get(root+server_file_name); Files.write(path, arr);
-	 * 
-	 * // 파일명, idx 를 Image 테이블에 추가
-	 * dao.uploadProfileImg(idx,server_file_name,user_no); } }
-	 */
-	/*
-	 * public HashMap<String, Object> list(String pagePerNum, String page) { int ppn
-	 * = Integer.parseInt(pagePerNum); int p = Integer.parseInt(page);
-	 * 
-	 * int offset = (p-1)*ppn; ArrayList<MyPageDTO> list = dao.list(ppn,offset);
-	 * 
-	 * HashMap<String, Object> map = new HashMap<String, Object>();
-	 * 
-	 * int pages = dao.totalPage(ppn); logger.info("만들 수 있는 총 페이지 갯수 : "+pages);
-	 * map.put("pages",pages); // 만들 페이지 수 map.put("list", list);
-	 * 
-	 * return map; }
-	 */
-
-
-
 	
+	MyPageDTO dto = new MyPageDTO(); 
+	
+	 public void imgUpload(MultipartFile photo, Integer user_no) throws IOException {
+	  
+		 dto.setIdx(user_no);
+		 dto.setCategory("회원");
+		 
+		 dao.uploadProfile(dto);
+	
+	 int image_id = dto.getImage_id(); // dto 에 방금 insert 된 image_id 가져오기
+	 logger.info("image_id:"+image_id);
+	 
+	 saveImage(photo,image_id,user_no);
+	 
+	 }
+	 
+		
+	  // 프로필 이미지 imageTable에 저장 
+	  private void saveImage(MultipartFile photo, int image_id, Integer user_no)
+	  throws IOException {
+	  
+	  String oriFileName = photo.getOriginalFilename(); 
+	  logger.info(oriFileName);
+	  
+	  if(!oriFileName.equals("")) { 
+		  String ext = oriFileName.substring(oriFileName.lastIndexOf(".")); 
+		  String server_file_name = System.currentTimeMillis()+ext; 
+		  logger.info(server_file_name);
+	  byte[] arr = photo.getBytes(); 
+	  Path path = Paths.get(root+server_file_name);
+	  Files.write(path, arr); 
+	  logger.info("path:"+path); 
+	  
+	  dao.saveProfileImg(image_id,server_file_name,user_no);
+	  } 
+	 }
 
+	 // 프로필 이미지 수정 
+	 public void imgModify(MultipartFile photo, Integer user_no) throws IOException {
+		 
+		 String oriFileName = photo.getOriginalFilename(); 
+		  logger.info(oriFileName);
+		  
+		  if(!oriFileName.equals("")) { 
+			  String ext = oriFileName.substring(oriFileName.lastIndexOf(".")); 
+			  String server_file_name = System.currentTimeMillis()+ext; 
+			  logger.info(server_file_name);
+		  byte[] arr = photo.getBytes(); 
+		  Path path = Paths.get(root+server_file_name);
+		  Files.write(path, arr); 
+		  logger.info("path:"+path); 
+		  
+		  dao.imgModify(server_file_name,user_no);
 
-
+			 
+	 }
+	}
+			 
+	
+		 
+		 
+	  
+/*
+ * public HashMap<String, Object> list(String pagePerNum, String page) { int ppn
+ * = Integer.parseInt(pagePerNum); int p = Integer.parseInt(page);
+ * 
+ * int offset = (p-1)*ppn; ArrayList<MyPageDTO> list = dao.list(ppn,offset);
+ * 
+ * HashMap<String, Object> map = new HashMap<String, Object>();
+ * 
+ * int pages = dao.totalPage(ppn); logger.info("만들 수 있는 총 페이지 갯수 : "+pages);
+ * map.put("pages",pages); // 만들 페이지 수 map.put("list", list);
+ * 
+ * return map; }
+ */
+	 
 
 }
